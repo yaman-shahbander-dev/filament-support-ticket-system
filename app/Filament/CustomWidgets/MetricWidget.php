97a29ed0a029b7893cf9617329fe6e5b62db9faf
yaman <yaman.dev3@gmail.com>
2024-06-29 19:18:41 +0300

    master: work on adding a custom widget

---
+++ app/Filament/CustomWidgets/MetricWidget.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Filament\CustomWidgets;
+
+use Filament\Support\Enums\IconPosition;
+use Filament\Widgets\Widget;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Str;
+use Illuminate\View\ComponentAttributeBag;
+
+class MetricWidget extends Widget
+{
+    protected static string $view = 'filament.custom-widgets.metric-widget';
+
+    public ?string $filter = null;
+
+//    protected ?array $chart = null;
+//
+//    /**
+//     * @var string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
+//     */
+//    protected string | array | null $chartColor = null;
+
+    /**
+     * @var string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
+     */
+    protected string | array | null $color = null;
+
+    protected ?string $icon = null;
+
+    protected string | Htmlable | null $description = null;
+
+    protected ?string $descriptionIcon = null;
+
+    protected IconPosition | string | null $descriptionIconPosition = null;
+
+    /**
+     * @var string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
+     */
+    protected string | array | null $descriptionColor = null;
+
+    /**
+     * @var array<string, scalar>
+     */
+    protected array $extraAttributes = [];
+
+    protected ?string $id = null;
+
+    protected string | Htmlable $label;
+
+    /**
+     * @var scalar | Htmlable | Closure
+     */
+    protected $value;
+
+    protected function getFilters(): ?array
+    {
+        return null;
+    }
+
+    /**
+     * @return array<float> | null
+     */
+    public function getChart(): ?array
+    {
+        return $this->chart;
+    }
+
+    /**
+     * @return string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
+     */
+    public function getChartColor(): string | array | null
+    {
+        return $this->chartColor ?? $this->color;
+    }
+
+    /**
+     * @return string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
+     */
+    public function getColor(): string | array | null
+    {
+        return $this->color;
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+    public function getDescription(): string | Htmlable | null
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
+     */
+    public function getDescriptionColor(): string | array | null
+    {
+        return $this->descriptionColor ?? $this->color;
+    }
+
+    public function getDescriptionIcon(): ?string
+    {
+        return $this->descriptionIcon;
+    }
+
+    public function getDescriptionIconPosition(): IconPosition | string
+    {
+        return $this->descriptionIconPosition ?? IconPosition::After;
+    }
+
+    /**
+     * @return array<string, scalar>
+     */
+    public function getExtraAttributes(): array
+    {
+        return $this->extraAttributes;
+    }
+
+    public function getExtraAttributeBag(): ComponentAttributeBag
+    {
+        return new ComponentAttributeBag($this->getExtraAttributes());
+    }
+
+    public function getLabel(): string | Htmlable
+    {
+        return $this->label;
+    }
+
+    public function getId(): string
+    {
+        return $this->id ?? Str::slug($this->getLabel());
+    }
+
+    /**
+     * @return scalar | Htmlable | Closure
+     */
+    public function getValue()
+    {
+        return value($this->value);
+    }
+
+    public function toHtml(): string
+    {
+        return $this->render()->render();
+    }
+
+    public function generateDataChecksum(): string
+    {
+        return md5(json_encode($this->getChart()) . now());
+    }
+}
---
+++ resources/views/filament/custom-widgets/metric-widget.blade.php
@@ -0,0 +1,80 @@
+@php
+    use Filament\Support\Enums\IconPosition;
+    use Filament\Support\Facades\FilamentView;
+
+    $descriptionColor = $this->getDescriptionColor() ?? 'gray';
+    $descriptionIcon = $this->getDescriptionIcon();
+    $descriptionIconPosition = $this->getDescriptionIconPosition();
+    $dataChecksum = $this->generateDataChecksum();
+    $tag = 'div';
+
+    $descriptionIconClasses = \Illuminate\Support\Arr::toCssClasses([
+        'fi-wi-stats-overview-stat-description-icon h-5 w-5',
+        match ($descriptionColor) {
+            'gray' => 'text-gray-400 dark:text-gray-500',
+            default => 'text-custom-500',
+        },
+    ]);
+
+    $descriptionIconStyles = \Illuminate\Support\Arr::toCssStyles([
+        \Filament\Support\get_color_css_variables($descriptionColor, shades: [500], alias: 'widgets::stats-overview-widget.stat.description.icon') => $descriptionColor !== 'gray',
+    ]);
+@endphp
+
+<{!! $tag !!}
+{{ $this->getExtraAttributeBag()->class([
+    'fi-wi-stats-overview-stat relative rounded-xl bg-white p-6 shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10',
+]) }}>
+<div class="grid gap-y-2">
+    <div class="flex items-center justify-between gap-x-2">
+        @if ($icon = $this->getIcon())
+            <x-filament::icon :icon="$icon"
+                              class="fi-wi-stats-overview-stat-icon h-5 w-5 text-gray-400 dark:text-gray-500" />
+        @endif
+
+        <span class="text-sm font-medium text-gray-500 dark:text-gray-400">
+                {{ $this->getLabel() }}
+            </span>
+
+        @if ($filters = $this->getFilters())
+            <x-filament::input.wrapper>
+                <x-filament::input.select wire:model.live="filter">
+                    @foreach ($filters as $value => $label)
+                        <option value="{{ $value }}">
+                            {{ $label }}
+                        </option>
+                    @endforeach
+                </x-filament::input.select>
+            </x-filament::input.wrapper>
+        @endif
+    </div>
+
+    <div class="text-3xl font-semibold tracking-tight text-gray-950 dark:text-white">
+        {{ $this->getValue() }}
+    </div>
+
+    @if ($description = $this->getDescription())
+        <div class="flex items-center gap-x-1">
+            @if ($descriptionIcon && in_array($descriptionIconPosition, [IconPosition::Before, 'before']))
+                <x-filament::icon :icon="$descriptionIcon" :class="$descriptionIconClasses" :style="$descriptionIconStyles" />
+            @endif
+
+            <span @class([
+                    'fi-wi-stats-overview-stat-description text-sm',
+                    match ($descriptionColor) {
+                        'gray' => 'fi-color-gray text-gray-500 dark:text-gray-400',
+                        default => 'fi-color-custom text-custom-600 dark:text-custom-400',
+                    },
+                ]) @style([
+                    \Filament\Support\get_color_css_variables($descriptionColor, shades: [400, 600], alias: 'widgets::stats-overview-widget.stat.description') => $descriptionColor !== 'gray',
+                ])>
+                    {{ $description }}
+                </span>
+
+            @if ($descriptionIcon && in_array($descriptionIconPosition, [IconPosition::After, 'after']))
+                <x-filament::icon :icon="$descriptionIcon" :class="$descriptionIconClasses" :style="$descriptionIconStyles" />
+            @endif
+        </div>
+    @endif
+</div>
+</{!! $tag !!}>
